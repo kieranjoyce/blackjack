8c2f1f0fe0fa2a82140ea14adf48124e3ff5b3b7
kieranjoyce <kieran.joyce99@gmail.com>
2022-12-30 21:01:59 +0000

    add handling for evaluating score with aces

--- hand.js
+++ hand.js
@@ -14,7 +14,13 @@ class Hand {
 
     for (let card of this.cards) {
       let isPictureCard = ["J", "Q", "K"].includes(card.value);
-      newScore += isPictureCard ? 10 : parseInt(card.value);
+      let isAce = card.value === "A";
+
+      if (isAce) {
+        newScore += newScore + 11 < 21 ? 11 : 1;
+      } else {
+        newScore += isPictureCard ? 10 : parseInt(card.value);
+      }
     }
 
     this.score = newScore;
