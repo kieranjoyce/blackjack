3e8671131a761b366e286f1a5df9055035b12021
kieranjoyce <kieran.joyce99@gmail.com>
2023-01-02 20:31:24 +0000

    add name question and game setup

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 #!/usr/bin/env node
 
-const inquirer = require("inquirer");
+import inquirer from "inquirer";
+import Dealer from "./dealer.js";
+import Player from "./player.js";
 
-console.log("hi mom");
-// inquirer.prompt();
+let playerName;
+let player;
+let dealer;
+let gameOver = false;
+
+console.log("welcome to blackjack");
+async function askPlayerName() {
+  let answer = await inquirer.prompt([
+    {
+      type: "input",
+      name: "playerName",
+      message: "What would you like to be called?",
+      default: "Player",
+    },
+  ]);
+
+  playerName = answer.playerName;
+}
+
+function setupGame() {
+  // create dealer
+  dealer = new Dealer();
+  // create player with given name
+  player = new Player(playerName);
+  // deal 2 cards to players hand
+  dealer.dealCard(player.hand);
+  dealer.dealCard(player.hand);
+}
+
+await askPlayerName();
+// log greeting and explanation of game?
+console.log(`welcome ${playerName}`);
+
+setupGame();
+
+// show cards to player (text form at first)
+// hit or stand once
